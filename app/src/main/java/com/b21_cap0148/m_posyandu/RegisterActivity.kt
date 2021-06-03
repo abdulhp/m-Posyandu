b64528405ec55eb9fcf93da4c57894ee336311f4
abdulhp <hpabdul902@gmail.com>
2021-06-03 12:04:08 +0700

    Register UI

---
+++ app/src/main/java/com/b21_cap0148/m_posyandu/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.b21_cap0148.m_posyandu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
