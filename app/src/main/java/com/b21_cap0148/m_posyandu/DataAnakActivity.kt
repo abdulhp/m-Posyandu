352437e1f4e2255753d01d23c7c261ad35ca1ac6
abdulhp <hpabdul902@gmail.com>
2021-06-06 11:52:07 +0700

    Data Anak UI

---
+++ app/src/main/java/com/b21_cap0148/m_posyandu/DataAnakActivity.kt
@@ -0,0 +1,11 @@
+package com.b21_cap0148.m_posyandu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataAnakActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_anak)
+    }
+}
